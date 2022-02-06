9a3597f2492f3114cccb4f6fab5cf1e5ed803096
tanhdmt <dmtclimax@gmail.com>
2022-02-06 17:10:35 +0700

    edit config

--- config/index.ts
+++ config/index.ts
@@ -1,11 +1,17 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 async function connect() {
     try {
         const uri = process.env.MONGO_URI;
-        await mongoose.connect(uri);
-        console.log("MongoDB database connection established successfully");
+        mongoose.connect(
+            uri,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            },
+            () => console.log("MongoDB Connected")
+        );
     } catch (error) {
         console.log(error);
     }
