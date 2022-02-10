669f6ae719bc89e67248557da1c7a6a2f4d8c9c9
tanhdmt <dmtclimax@gmail.com>
2022-02-10 21:38:21 +0700

    fix connect

--- routers/page.ts
+++ routers/page.ts
@@ -1,9 +1,11 @@
 export {};
 const express = require("express");
 const cache = require("express-redis-cache")({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
-    auth_pass: process.env.REDIS_PASSWORD,
+    host: process.env.REDIS_HOST || "ec2-3-89-15-171.compute-1.amazonaws.com",
+    port: process.env.REDIS_PORT || 29540,
+    auth_pass:
+        process.env.REDIS_PASSWORD ||
+        "pcaa8c827f7e852939a1fa02bdd88c3f644275443f56257250ac462a716630eec",
 });
 // const cache = require("express-redis-cache")();
 const router = express.Router();
